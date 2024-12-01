ebf70d5ddb8e309b5b801b8fb40287cb7ffffa78
maxto <max.toepfer.taucha@gmail.com>
2024-12-01 10:53:08 +0100

    add ordered output & renamed count functions

--- src/main/scala/ai/scads/odibel/datasets/wikitext/eval/TKGUtils.scala
+++ src/main/scala/ai/scads/odibel/datasets/wikitext/eval/TKGUtils.scala
@@ -9,23 +9,24 @@ import org.apache.spark.sql.functions._
 object TKGUtils {
 
   // Function: Calculate all unique windows
-  def calculateAllUniqueWindows(data: Dataset[CSVRow]): Long = {
+  def countAllUniqueWindows(data: Dataset[CSVRow]): Long = {
     import data.sparkSession.implicits._
     data.map(row => row.tFrom.toString + row.tUntil.toString: String).distinct().count()
   }
 
   // Function: Count triples per subject
   def countTriplesPerSubject(data: Dataset[CSVRow]): DataFrame = {
-    data.select("head", "rel", "tail") // Select only the relevant fields
-      .distinct() // Remove duplicates
-      .groupBy("head") // Group by subject (head)
+    data.select("head", "rel", "tail")
+      .orderBy("head", "rel", "tail").distinct()
+      .groupBy("head")
       .agg(
-        count("*").as("triple_count") // Count unique triples per subject
+        countDistinct("*").as("triple_count") // Count unique triples per subject
       )
+      .orderBy("head")
   }
 
   // Function: Revisions per page
-  def calculateRevisionsPerPage(data: Dataset[CSVRow]): DataFrame = {
+  def countRevisionsPerPage(data: Dataset[CSVRow]): DataFrame = {
     import data.sparkSession.implicits._
 
     // 1. Filter pages with wikiPageID relation and rename 'head' to 'page'
@@ -43,22 +44,24 @@ object TKGUtils {
       .agg(
         count("*").as("revision_count") // Count the number of revisions for each page
       )
+      .orderBy("head")
   }
 
   // Function: Changes per predicate
-  def calculateChangesPerPredicate(data: Dataset[CSVRow]): DataFrame = {
+  def countChangesPerPredicate(data: Dataset[CSVRow]): DataFrame = {
     data.groupBy("head", "rel")
       .agg(
         countDistinct("tail").as("unique_changes"),
         countDistinct("rFrom", "rUntil").as("all_changes")
       )
+      .orderBy("head", "rel")
   }
 
   // Function: Snapshot creation for a given time window
   def createSnapshot(data: Dataset[CSVRow], timestamp: Long, outputPath: Option[String] = None): DataFrame = {
 
     // 1. Filter the data for the given time window
-    val snapshot = data.filter(row => row.tFrom <= timestamp && timestamp <= row.tUntil)
+    val snapshot = data.filter(row => row.tFrom <= timestamp && timestamp < row.tUntil)
 
     // 2. Dynamically reorder columns based on CSVRow field order
     val fieldOrder = classOf[CSVRow].getDeclaredFields.map(_.getName)
