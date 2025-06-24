7088029d1aa18f8610db0dbbda7ddf371064cd45
Theo <gg46ixav@studserv.uni-leipzig.de>
2025-06-24 10:25:56 +0200

    added CSVToRDFStar

---
+++ src/main/scala/ai/scads/odibel/datasets/wikitext/transform/CSVToRDFStar.scala
@@ -0,0 +1,17 @@
+package ai.scads.odibel.datasets.wikitext.transform
+
+object CSVToRDFStar extends CSVToRDF {
+  override def convertRowToRDF(line: String): String = {
+    SerUtil.readCsvLine(line) match {
+      case Some(SerUtil.RDFTriple(head, rel, literal, langTagOpt, tStart, tEnd, rStart, rEnd)) =>
+        val langTagStr = langTagOpt.getOrElse("")
+        s"""<< <$head> <$rel> "$literal"$langTagStr >>
+           |    rel:tStart "$tStart"^^xsd:dateTime ;
+           |    rel:tEnd "$tEnd"^^xsd:dateTime ;
+           |    rel:rStart "$rStart"^^xsd:dateTime ;
+           |    rel:rEnd "$rEnd"^^xsd:dateTime .
+           |""".stripMargin
+      case None => ""
+    }
+  }
+}
