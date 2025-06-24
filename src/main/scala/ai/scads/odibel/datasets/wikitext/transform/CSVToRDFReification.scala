82baabf89d99b784229735c9d7b5e8d357dd339a
Theo <gg46ixav@studserv.uni-leipzig.de>
2025-06-24 10:33:26 +0200

    object is not literal if it contains "http" + refactoring

--- src/main/scala/ai/scads/odibel/datasets/wikitext/transform/CSVToRDFReification.scala
+++ src/main/scala/ai/scads/odibel/datasets/wikitext/transform/CSVToRDFReification.scala
@@ -3,12 +3,11 @@ package ai.scads.odibel.datasets.wikitext.transform
 object CSVToRDFReification extends CSVToRDF {
   override def convertRowToRDF(line: String): String = {
     SerUtil.readCsvLine(line) match {
-      case Some(SerUtil.RDFTriple(head, rel, literal, langTagOpt, tStart, tEnd, rStart, rEnd)) =>
-        val langTagStr = langTagOpt.getOrElse("")
+      case Some(SerUtil.RDFTriple(head, rel, obj, tStart, tEnd, rStart, rEnd)) =>
         s"""_:b${System.nanoTime()} a rdf:Statement ;
            |    rdf:subject <$head> ;
            |    rdf:predicate <$rel> ;
-           |    rdf:object "$literal"$langTagStr ;
+           |    rdf:object $obj ;
            |    rel:tStart "$tStart"^^xsd:dateTime ;
            |    rel:tEnd "$tEnd"^^xsd:dateTime ;
            |    rel:rStart "$rStart"^^xsd:dateTime ;
