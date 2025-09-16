b6a56a21c784af693038e884cb55f83fb1c6ef71
Theo <gg46ixav@studserv.uni-leipzig.de>
2025-09-16 12:06:40 +0200

    added CSVToParquet.scala

---
+++ src/main/scala/ai/scads/odibel/datasets/wikitext/transform/CSVToParquet.scala
@@ -0,0 +1,25 @@
+package ai.scads.odibel.datasets.wikitext.transform
+
+import ai.scads.odibel.datasets.wikitext.utils.SparkSessionUtil
+
+object CSVToParquet extends App {
+
+  if (args.length < 2) {
+    System.err.println("Usage: CSVToParquet <input_csv> <output_parquet>")
+    System.exit(1)
+  }
+
+  val inputPath = args(0)
+  val outputPath = args(1)
+
+  private val sql = SparkSessionUtil.sql
+
+  sql.read
+    .option("header", "true")
+    .option("inferSchema", "true")
+    .csv(inputPath)
+    .write
+    .parquet(outputPath)
+
+}
+
