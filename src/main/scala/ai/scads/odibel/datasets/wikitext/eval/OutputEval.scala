498cf49bda4f214ed880b7435e84b4ea13b10c31
Theo <gg46ixav@studserv.uni-leipzig.de>
2025-09-17 10:05:05 +0200

    added repartition(100)

--- src/main/scala/ai/scads/odibel/datasets/wikitext/eval/OutputEval.scala
+++ src/main/scala/ai/scads/odibel/datasets/wikitext/eval/OutputEval.scala
@@ -31,7 +31,7 @@ class OutputEval extends Callable[Int] {
   import sql.implicits._
 
   def writeOut(name: String, ds: DataFrame): Unit = {
-    ds
+    ds.repartition(100)
       .write
       .mode("overwrite")
       .option("header", "true")
