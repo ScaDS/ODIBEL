0960faeed3114e7cfbe2ffce896ec655aa8d8310
Theo <gg46ixav@studserv.uni-leipzig.de>
2025-10-06 12:21:30 +0200

    added some logging for DBpediaTKGExtraction

--- src/main/scala/ai/scads/odibel/datasets/wikitext/DBpediaTKGExtraction.scala
+++ src/main/scala/ai/scads/odibel/datasets/wikitext/DBpediaTKGExtraction.scala
@@ -7,9 +7,13 @@ import ai.scads.odibel.datasets.wikitext.extraction.{Executor, ExtractionJob}
 import ai.scads.odibel.datasets.wikitext.log.{EventLogger, HeartbeatMonitor}
 import ai.scads.odibel.datasets.wikitext.utils.WikiUtil
 import ai.scads.odibel.utils.HDFSUtil
+<<<<<<< HEAD
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileStatus, FileSystem, Path}
 import org.apache.hadoop.io.compress.CompressionCodecFactory
+=======
+import org.slf4j.LoggerFactory
+>>>>>>> 1316b45 (added some logging for DBpediaTKGExtraction)
 
 import java.io.{BufferedReader, File, InputStream}
 import java.nio.file.Paths
@@ -21,9 +25,13 @@ import scala.io.{BufferedSource, Source}
  */
 class DBpediaTKGExtraction {
 
-  def processStream(iterator: Iterator[String], diefUrl: String): Iterator[TemporalExtractionResult] = {
-    processPageRevisionIterator(WikiUtil.splitToItem(iterator).map(WikiUtil.enrichFlatRawPageRevision), diefUrl)
+  def processStream(iterator: Iterator[String], diefUrl: String): (Iterator[TemporalExtractionResult], Int) = {
+    processPageRevisionIterator(
+      WikiUtil.splitToItem(iterator).map(WikiUtil.enrichFlatRawPageRevision),
+      diefUrl
+    )
   }
+
 
   def process(source: String, sink: String, endpoints: List[String]): Unit = {
 
@@ -119,31 +127,63 @@ class DBpediaTKGExtraction {
 
 object DBpediaTKGExtraction {
 
-  def processPageRevisionIterator(pageRevisionIterator: Iterator[PageRevision], diefEndpoint: String): Iterator[TemporalExtractionResult] = {
+  private val logger = LoggerFactory.getLogger(getClass)
+
+  def processPageRevisionIterator(pageRevisionIterator: Iterator[PageRevision], diefEndpoint: String): (Iterator[TemporalExtractionResult], Int) = {
     val rc = new RCDiefServer(diefEndpoint)
     implicit var twb: TemporalWindowBuilder = new TemporalWindowBuilder()
     var oPageId = -1L
 
-    pageRevisionIterator.flatMap({
-      pageRevision =>
-        if (ProfileConfig.wikiNamespaceFilter.contains(pageRevision.ns.getOrElse(-1))) {
-          val tripleDoc = extractTripleDoc(rc, pageRevision)
+    var totalRevisions = 0
+    var failedExtractions = 0
+    var totalTriplesExtracted = 0
 
-          if (oPageId != pageRevision.pId) {
-            // TODO eventLog.logEvent(SucceededPageEvent(pageId,Map()))
-            oPageId = pageRevision.pId
-            val temporalResults: List[TemporalExtractionResult] = twb.addGraphVersion(List(),Long.MaxValue)(Long.MaxValue.toString) // twb.buildEntries()
-            twb = new TemporalWindowBuilder()
-            diffAndAppendWindow(tripleDoc, pageRevision)
-            temporalResults
-          } else {
-            diffAndAppendWindow(tripleDoc, pageRevision)
-          }
+    val results = pageRevisionIterator.flatMap { pageRevision =>
+      totalRevisions += 1
+
+      if (ProfileConfig.wikiNamespaceFilter.contains(pageRevision.ns.getOrElse(-1))) {
+        val tripleDoc = extractTripleDoc(rc, pageRevision)
+
+        if (tripleDoc.isEmpty) {
+          failedExtractions += 1
+          logger.warn(s"[WARN] No triples extracted for revisionId=${pageRevision.rId} (pageId=${pageRevision.pId})")
         } else {
-          None
+          logger.info(s"[OK] Extracted ${tripleDoc.size} triples for revisionId=${pageRevision.rId}")
+          totalTriplesExtracted += tripleDoc.size
         }
-    }) ++ twb.addGraphVersion(List(),Long.MaxValue)(Long.MaxValue.toString) // TODO check
+
+        if (oPageId != pageRevision.pId) {
+          oPageId = pageRevision.pId
+          val temporalResults = twb.addGraphVersion(List(), Long.MaxValue)(Long.MaxValue.toString)
+          twb = new TemporalWindowBuilder()
+
+          val addedTriples = diffAndAppendWindow(tripleDoc, pageRevision)
+          if (addedTriples.nonEmpty) {
+            logger.info(s"[NEW] ${addedTriples.size} new triples for pageId=${pageRevision.pId}, revisionId=${pageRevision.rId}")
+          }
+
+          temporalResults
+        } else {
+          val addedTriples = diffAndAppendWindow(tripleDoc, pageRevision)
+          if (addedTriples.nonEmpty) {
+            logger.debug(s"[DIFF] ${addedTriples.size} triples changed for revisionId=${pageRevision.rId}")
+          }
+          addedTriples
+        }
+      } else {
+        None
+      }
+    }
+
+    logger.info(s"=== Extraction finished ===")
+    logger.info(s"Total revisions processed: $totalRevisions")
+    logger.info(s"Failed extractions: $failedExtractions")
+    logger.info(s"Total triples extracted: $totalTriplesExtracted")
+
+    ((results ++ twb.addGraphVersion(List(), Long.MaxValue)(Long.MaxValue.toString)), failedExtractions)
   }
+
+
 
   def diffAndAppendWindow(triples: Option[List[String]],pageRevision: PageRevision)(implicit twb: TemporalWindowBuilder): List[TemporalExtractionResult] = {
     if(triples.isDefined) {
